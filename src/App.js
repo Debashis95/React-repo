c50d23dc6bbb98fb120ad9701ea0b9815543029e
Debashis95 <101383976+Debashis95@users.noreply.github.com>
2022-05-21 15:03:32 +0530

    First Commit
    For internship project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UseEffectApi from './components/useEffect/UseEffectApi';
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectApi />
+    </div>
+  );
+}
+
+export default App;
